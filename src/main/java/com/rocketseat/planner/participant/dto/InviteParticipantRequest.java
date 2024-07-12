1bcdcb8f24c4124cde7328f5519f96ef7bd35282
João Victor Bezerra da Silva <dev.joaovictor@hotmail.com>
2024-07-12 10:59:41 +0100

    feat: add invite participant method

---
+++ src/main/java/com/rocketseat/planner/participant/dto/InviteParticipantRequest.java
@@ -0,0 +1,6 @@
+package com.rocketseat.planner.participant.dto;
+
+public record InviteParticipantRequest(
+      String email
+) {
+}
---
+++ src/main/java/com/rocketseat/planner/participant/dto/InviteParticipantResponse.java
@@ -0,0 +1,8 @@
+package com.rocketseat.planner.participant.dto;
+
+import java.util.UUID;
+
+public record InviteParticipantResponse(
+      UUID id
+) {
+}
